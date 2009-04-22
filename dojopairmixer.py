7aa4a5fc511b7ae9eda5fd08278cc7355237ecae
Rodolfo Carvalho <rhcarvalho@gmail.com>
2009-04-21 22:16:50 -0300

    about to write some user interaction code

--- dojopairmixer.py
+++ dojopairmixer.py
@@ -1,17 +1,26 @@
 import random
 from itertools import cycle
 
-def sort_people(people, minutes_per_dojo, minutes_per_turn):
+
+def sort_people(people, minutes_per_dojo, minutes_per_turn=7):
     number_of_turns = minutes_per_dojo // minutes_per_turn
     
     random.shuffle(people)
     
     turns = []
-    
     people_iterator = cycle(people)
     pilot, copilot = people_iterator.next(), people_iterator.next()
+    
     while len(turns) < number_of_turns:
         turns.append((pilot, copilot))
         pilot, copilot = copilot, people_iterator.next()
     
     return turns
+
+
+def _main():
+    pass
+
+
+if __name__ == '__main__':
+    _main()
